6b5b1762f361c4437cf6fcfc9a707c364f6bfbab
Angelise Perez <angelisep3@nycstudents.net>
2025-11-14 12:39:33 -0500

    excersise2 - jspt1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let main1 = document.getElementById("one");
 
+const posts = [
+    { image: "",
+      title: "",
+      post: ""
+    }
+]
+
+const container = document.getElementbyID("myContainer");
+
+posts.forEach(obj => {
+  const htmlString = `
+    <div class="person-card">
+      <h3>${obj.name}</h3>
+    </div>
+  `;
+  container.insertAdjacentHTML("beforeend", htmlString);
+});
