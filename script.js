c3773b329c75ada6f8400d96eee09ec5bb040bfd
Angelise Perez <angelisep3@nycstudents.net>
2025-11-17 12:43:03 -0500

    js done

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-let main1 = document.getElementById("one");
+const container = document.getElementById("myContainer");
 
 const posts = [
-    { image: "",
-      title: "",
-      post: ""
+    { image: "https://i.pinimg.com/736x/fc/22/9b/fc229be3e7049e4a14ce9216640140f5.jpg",
+      title: "Kali Uchis",
+      post: "Kali Uchis is a colombian singer who produces a mix of multiple genres such as, pop, R&B and reggaeton music."
     }
-]
+];
 
-const container = document.getElementbyID("myContainer");
 
-posts.forEach(obj => {
-  const htmlString = `
-    <div class="person-card">
-      <h3>${obj.name}</h3>
+posts.forEach(item => {
+  const htmlContent = `
+    <div class="myContainer">
+    <h2>Title: ${item.title}</h2>
+      <img src=${item.image}>
+      <p>Desc: ${item.post}</p>
     </div>
   `;
-  container.insertAdjacentHTML("beforeend", htmlString);
+  container.insertAdjacentHTML("beforeend", htmlContent);
+
+console.log('Generated HTML for:', item.name);
+    console.log(htmlContent);
 });
