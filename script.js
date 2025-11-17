ff233e6a08d06ba095a2c6becda44cf03896c236
Angelise Perez <angelisep3@nycstudents.net>
2025-11-17 12:47:05 -0500

    array done

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const posts = [
     { image: "https://i.pinimg.com/736x/fc/22/9b/fc229be3e7049e4a14ce9216640140f5.jpg",
       title: "Kali Uchis",
       post: "Kali Uchis is a colombian singer who produces a mix of multiple genres such as, pop, R&B and reggaeton music."
+    },
+    { image: "https://imageio.forbes.com/blogs-images/ogdenpayne/files/2017/02/Daniel-Caesar-NEw-by-Keith-Henry.jpg?format=jpg&height=600&width=1200&fit=bounds",
+      title: "Daniel Caeser",
+      post: "Daneil Caeser is a black singer who produces a genre such as, R&B."
+    },
+    { image: "https://upload.wikimedia.org/wikipedia/en/4/4b/Ariana_Grande_-_Dangerous_Woman_%28Official_Album_Cover%29.png",
+      title: "Ariana Grande",
+      post: "Ariana Grande is a singer who produces a mix of multiple genres such as, pop, and R&B music."
     }
 ];
 
